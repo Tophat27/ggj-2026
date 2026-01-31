5765eac89606587940caa38995e2faa82581a5df
ElissonXD <elissondias2006@gmail.com>
2026-01-31 17:35:02 -0300

    masks and box

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -3,9 +3,24 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	[Export]
+	public float Speed = 300.0f;
+	[Export]
+	public float JumpVelocity = -400.0f;
+
+	[Export]
+	public float PushForce = 15f;
+	public float MinPush = 10f;
+
+	[Export]
+	int current_sprite = 0; // 0 - player, 1 - raposa, 2 - ala ursa, 3 - ouriço
 	bool isJumping = false;
+	[Export]
+	int totaljumps = 1;
+	int currentjumps = 1;
+
+	[Export]
+	CollisionShape2D force;
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -19,24 +34,25 @@ public partial class Player : CharacterBody2D
 			velocity += GetGravity() * (float)delta;
 			if (!isJumping)
 			{
-				animatedSprite2D.Animation = "falling";
+				animatedSprite2D.Animation = "falling_" + current_sprite.ToString();
 			}
 		}
 
 		if (IsOnFloor() && isJumping)
 		{
 			isJumping = false;
-			animatedSprite2D.Animation = "landing";
+			animatedSprite2D.Animation = "landing_" + current_sprite.ToString();
 			GD.Print("im here");
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("ui_accept") && (IsOnFloor() || currentjumps > 0))
 		{
 			velocity.Y = JumpVelocity;
-			animatedSprite2D.Animation = "jump";
+			animatedSprite2D.Animation = "jump_" + current_sprite.ToString();
 			jumptimer.Start();	
 			isJumping = true;	
+			currentjumps -= 1;
 		}
 
 
@@ -48,7 +64,8 @@ public partial class Player : CharacterBody2D
 			velocity.X = direction.X * Speed;
 			
 			if (IsOnFloor() && !isJumping){
-				animatedSprite2D.Animation = "walk";
+				animatedSprite2D.Animation = "walk_" + current_sprite.ToString();
+				currentjumps = totaljumps;
 			}
 			animatedSprite2D.FlipH = velocity.X < 0;
 		}
@@ -57,17 +74,32 @@ public partial class Player : CharacterBody2D
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			
 			if (IsOnFloor() && !isJumping){
-				animatedSprite2D.Animation = "idle";
+				animatedSprite2D.Animation = "idle_" + current_sprite.ToString();
+				currentjumps = totaljumps;
 			}
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
-		GD.Print(animatedSprite2D.Animation);
 
 		if (!isJumping)
 		{
 			jumptimer.Stop();
+		}
+
+		getCollision();
+	}
+	
+	
+	public void getCollision(){
+		for (int i = 0; i < GetSlideCollisionCount(); i++)
+		{
+			var c = GetSlideCollision(i);
+			if (c.GetCollider() is RigidBody2D rb)
+			{
+				float push = PushForce * Velocity.Length() / Speed + MinPush;
+				rb.ApplyCentralImpulse(-c.GetNormal() * push);
+			} 
 		}
 	}
 
@@ -76,4 +108,16 @@ public partial class Player : CharacterBody2D
 		isJumping = false;
 	}
 
+	public void ActivateFoxMask()
+	{
+		current_sprite = 1;
+		totaljumps = 2;
+		force.Disabled = true;
+	}
+	
+	public void ActivateBearMask(){
+		current_sprite = 2;
+		totaljumps = 1;
+		force.Disabled = false;
+	}
 }
